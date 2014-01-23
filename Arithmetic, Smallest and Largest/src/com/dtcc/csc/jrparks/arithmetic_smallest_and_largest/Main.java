8d3eaa705a0d83c525cffdb2c4ce114f6cc13df5
James R Parks <jarijop@gmail.com>
2014-01-23 16:58:30 -0500

    Changed BufferedInput to Scanner

--- Arithmetic, Smallest and Largest/src/com/dtcc/csc/jrparks/arithmetic_smallest_and_largest/Main.java
+++ Arithmetic, Smallest and Largest/src/com/dtcc/csc/jrparks/arithmetic_smallest_and_largest/Main.java
@@ -3,23 +3,22 @@
  */
 package com.dtcc.csc.jrparks.arithmetic_smallest_and_largest;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
  * @author jrparks
  * 
  */
 public class Main {
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		System.out.println("DTCC CSC 151 450 SP14 - JRParks");
 		System.out.println("Arithmetic, Smallest and Largest");
-		
-		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+		Scanner input = new Scanner(System.in);
 		Integer n1 = readInteger(input), n2 = readInteger(input), n3 = readInteger(input);
 		Integer[] asw = doMaths(n1, n2, n3);
 		System.out.printf("Sum: %d\n", asw[0]);
@@ -28,26 +27,26 @@ public class Main {
 		System.out.printf("Smallest: %d\n", asw[3]);
 		System.out.printf("Largest: %d\n", asw[4]);
 	}
-	
+
 	/**
 	 * 
 	 * @param input
 	 *            - BufferedReader for input
 	 * @return
 	 */
-	public static Integer readInteger(BufferedReader input) {
+	public static Integer readInteger(Scanner input) {
 		Integer n = null;
 		while (n == null) {
 			try {
 				System.out.print("Enter an integer:");
-				n = Integer.parseInt(input.readLine());
+				n = input.nextInt();
 			} catch (Exception ioex) {
 				System.out.println("You did not enter a number.\nPlease try again.");
 			}
 		}
 		return n;
 	}
-	
+
 	/**
 	 * 
 	 * @param ns
