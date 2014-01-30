6368f6c644a9fb2db1ca1171935742e0f78b91d1
James R Parks <jarijop@gmail.com>
2014-01-30 16:11:14 -0500

    Added extra to Employee Class Project

--- Employee Class/src/com/dtcc/csc/jrparks/employee/EmployeeTest.java
+++ Employee Class/src/com/dtcc/csc/jrparks/employee/EmployeeTest.java
@@ -10,27 +10,35 @@ import java.util.LinkedList;
  * 
  */
 public class EmployeeTest {
-	private LinkedList<Employee>	Employees;
-	
+	private LinkedList<Employee> Employees;
+
 	public EmployeeTest() {
 		Employees = new LinkedList<Employee>();
 		Employees.add(new Employee());
 		Employees.add(new Employee("Jane", "Haberdasher", 1200.00));
 	}
-	
+
 	/**
-	 * 
+	 * Print the list of employees
 	 */
 	public void printEmployeeList() {
 		for (Employee employee : this.Employees)
 			System.out.println(employee);
 	}
-	
+
 	/**
-	 * 
+	 * Give the default raise to all employees
 	 */
 	public void giveRaises() {
 		for (Employee employee : this.Employees)
 			employee.giveRaise();
+	}
+
+	/**
+	 * Give a defined raise to all employees
+	 */
+	public void giveRaises(Double percentAmount) {
+		for (Employee employee : this.Employees)
+			employee.giveRaise(percentAmount);
 	}
 }
